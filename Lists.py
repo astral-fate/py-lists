73197d3dcd340a35e98784bf92660a703a23fe12
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2022-01-28 23:11:00 +0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,12 @@
+a_list = [ ' a ' ];
+
+a_list = a_list + [ 2.0 , 3 ];
+
+ a_list.append ( True );
+
+ a_list.extend ( [ ' four ' , ' $ ' ] );
+
+a_list.insert ( 0 , ' $ ' );
+
+ print ( a_list [ 6 ] );
+
